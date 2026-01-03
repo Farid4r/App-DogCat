580b243791123e7b870860da2a97c6d1a54ee2df
Raquel. <168386309+Farid4r@users.noreply.github.com>
2026-01-03 14:19:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 import streamlit as st
 import tensorflow as tf
 import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
 from PIL import Image
 
 # Load model
 model = tf.keras.models.load_model("model_cat_dog.keras")
 
-st.set_page_config(page_title="Klasifikasi Kucing & Anjing")
+st.set_page_config(
+    page_title="Klasifikasi Kucing & Anjing",
+    layout="centered"
+)
 
-st.title("🐱🐶 Klasifikasi Gambar Kucing dan Anjing")
-st.write("Upload gambar kucing atau anjing untuk diprediksi oleh model.")
+st.title("Klasifikasi Gambar Kucing dan Anjing")
+st.write("Upload gambar kucing atau anjing untuk diprediksi oleh model Machine Learning.")
 
 uploaded_file = st.file_uploader(
     "Upload gambar",
@@ -18,12 +23,14 @@ uploaded_file = st.file_uploader(
 
 if uploaded_file is not None:
     img = Image.open(uploaded_file).convert("RGB")
-    st.image(img, caption="Gambar yang diupload", use_column_width=True)
+    st.image(img, caption="Gambar yang diupload", use_container_width=True)
 
+    # Preprocessing
     img = img.resize((150, 150))
     img_array = np.array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
 
+    # Prediksi
     result = model.predict(img_array)[0][0]
 
     if result < 0.5:
