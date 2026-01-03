4828d3369ea8fd191d936d6e46eef73a3a0a74e7
Raquel. <168386309+Farid4r@users.noreply.github.com>
2026-01-03 14:35:53 +0700

    Update app.py
    Eror di streamlit

--- app.py
+++ app.py
@@ -1,49 +1,40 @@
 import streamlit as st
 import tensorflow as tf
 import numpy as np
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
 from PIL import Image
 
-# Load model
 model = tf.keras.models.load_model("model_cat_dog.keras")
 
-st.set_page_config(
-    page_title="CatDog Classifier",
-    layout="centered"
-)
-
-st.title("Klasifikasi Gambar Kucing dan Anjing")
-st.write("Upload gambar kucing atau anjing untuk diprediksi oleh model Machine Learning.")
+st.title("Cat & Dog Classifier")
 
 uploaded_file = st.file_uploader(
-    "Upload gambar",
+    "Upload image",
     type=["jpg", "jpeg", "png"]
 )
 
+THRESHOLD = 70.0
+
 if uploaded_file is not None:
     img = Image.open(uploaded_file).convert("RGB")
-    st.image(img, caption="Gambar yang diupload", use_container_width=True)
+    st.image(img, caption="Uploaded Image", use_container_width=True)
 
-    # Preprocessing
     img = img.resize((150, 150))
     img_array = np.array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
 
-    # Prediksi
     result = model.predict(img_array)[0][0]
 
-if result < 0.5:
-    prediction = "Kucing"
-    confidence = (1 - result) * 100
-else:
-    prediction = "Anjing"
-    confidence = result * 100
+    if result < 0.5:
+        prediction = "Cat"
+        confidence = (1 - result) * 100
+    else:
+        prediction = "Dog"
+        confidence = result * 100
 
-confidence = round(confidence, 2)
+    confidence = round(confidence, 2)
 
-if confidence < THRESHOLD:
-    st.warning("Gambar tersebut bukan kucing maupun anjing.")
-else:
-    st.success(f"Hasil Prediksi: {prediction}")
-    st.write(f"Tingkat Keyakinan Model: {confidence}%")
+    if confidence < THRESHOLD:
+        st.warning("The image is neither a cat nor a dog.")
+    else:
+        st.success(f"Prediction: {prediction}")
+        st.write(f"Confidence: {confidence}%")
